009a971508de83f0738418391281750d9ead61e6
ChengfaGuo <guo4cheng8@outlook.com>
2019-07-29 11:30:37 +0800

    Update testBaidu.py
    search a word

--- testBaidu.py
+++ testBaidu.py
@@ -6,3 +6,7 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 # Open Baidu.com
 driver.get('https://www.baidu.com')
+
+# search a word e.g. 'selenium'
+driver.find_element_by_id('kw').send_keys('selenium')
+driver.find_element_by_id('su').click()
